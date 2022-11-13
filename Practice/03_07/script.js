45f84e17a64d03dcef63ca0b126f219f6793887b
r-scott25 <rachelbernal2@gmail.com>
2022-11-12 18:18:59 -0600

    object creation

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,19 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+ const chair = {
+  color:"brown",
+  wheels: 4,
+  comfy: true,
+}
+console.log(chair);
+console.log(chair.wheels);
+const desk = {
+  color: "brown",
+  shape: 'L',
+  drawers: 2,
+  shelves: 1,
+}
+console.log(desk);
+console.log(desk.color);
