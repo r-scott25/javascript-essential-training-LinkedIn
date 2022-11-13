2165fc19463c72c59a201898602513f186e2f0d2
r-scott25 <rachelbernal2@gmail.com>
2022-11-13 14:09:05 -0600

    querySelector and querySelectorAll

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,9 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+document.querySelector("main .backpack .backpack__name").style.color = "rosybrown";
+
+document
+  .querySelectorAll("main li")
+  .forEach((item) => (item.style.color = "green"));
