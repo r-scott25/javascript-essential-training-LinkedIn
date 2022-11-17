fa8dd876d6f89b677f285f6bef51fb84a6e20a80
r-scott25 <rachelbernal2@gmail.com>
2022-11-17 17:46:58 -0600

    event listener on button

--- 09_03/script.js
+++ 09_03/script.js
@@ -39,6 +39,15 @@ const backpackList = backpackObjectArray.map((backpack) => {
     <button class="lid-toggle">Open lid</button>
   `;
 
+  const button = backpackArticle.querySelector(".lid-toggle");
+  const status = backpackArticle.querySelector(".backpack_lid span");
+
+  button.addEventListener("click", (event) => {
+    console.log(event);
+    status.innerText === "open"
+      ? (status.innerText = "closed")
+      : (status.innerText = "open");
+  });
   return backpackArticle;
 });
 
